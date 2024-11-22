ff2a43eadd984e06766bb8c81130c88ccf6ea884
Vidalian <lrw9my@umsystem.edu>
2024-11-21 19:05:29 -0600

    Initial commit

---
+++ tests/Integration/src/states/IdleState.cc
@@ -0,0 +1,23 @@
+/******************************************************************************
+ * @brief Unit test for the Idle State.
+ *
+ * @file IdleState.cc
+ * @author Lucas Wiley (lrw9my@mst.edu)
+ * @date 2024-11-21
+ *
+ * @copyright Copyright MRDT 2024 - All Rights Reserved
+ ******************************************************************************/
+
+// #include "../../../../states/IdleState.h"
+#include "../../../../states/IdleState.cpp"
+
+/// \cond
+#include <gtest/gtest.h>
+
+/// \endcond
+
+ TEST(IdleStateTest, temp)
+ {
+
+ }
+
